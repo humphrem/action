bf179dec8c4a012877af587d2171ddd67c55af5e
David Humphrey <david.andrew.humphrey@gmail.com>
2023-08-23 10:14:48 -0400

    Working
    Refactor classes to extract a base class
    
    Add documentation
    
    Bug fixes, move box plotting into base detector
    
    Move draw_detections into detector
    
    Add ClipManager and utils, general clean-up
    
    Create detector only once per run, not per file
    
    Debug logging when cleaning up clip queue
    
    Add docs

---
+++ clip_manager.py
@@ -0,0 +1,166 @@
+"""
+This module is responsible for manage the creation of video clips. As
+a detector finds objects of interest, video clips are extracted. The majority
+of this work is done by the ClipManager class, with support from some other
+utility functions.
+"""
+
+
+import os
+import shutil
+import subprocess
+from multiprocessing import Process, Queue, Event
+from queue import Empty
+
+from utils import format_time
+
+
+def get_clips_dir(video_path):
+    """
+    Get the clips directory for a video path. We use the
+    video's basename + _clips (e.g., movie -> movie_clips/).
+
+    Args:
+        video_path (str): The path to the video file.
+
+    Returns:
+        str: The path to the clips directory.
+    """
+    return f"{os.path.splitext(video_path)[0]}_clips"
+
+
+def remove_clips_dir(video_path, logger):
+    """
+    Delete the old clips directory for the given video_pat,
+    if exists.
+
+    Args:
+        video_path (str): The path to the video file.
+        logger (Logger): The logger object.
+
+    Returns:
+        None
+    """
+    try:
+        clips_dir = get_clips_dir(video_path)
+        logger.debug(f"Removing old clips from {clips_dir}")
+        shutil.rmtree(clips_dir)
+    except Exception as e:
+        logger.warning(f"Unable to remove old clips: {e}")
+
+
+class ClipManager:
+    """
+    A class used to manage the creation of video clips. Video clips
+    are created using ffmpeg (must be installed on the system)
+    in a separate process.
+
+    Attributes:
+        logger (Logger): The logger object.
+        clip_queue (Queue): The queue of clips.
+        stop_event (Event): The stop event for the clip process.
+        clip_process (Process): The clip process.
+    """
+
+    def __init__(self, logger):
+        """
+        The constructor for the ClipManager class. It manages
+        the queue and process for ffmpeg.
+
+        Args:
+            logger (Logger): The logger object.
+        """
+        self.logger = logger
+        self.clip_queue = Queue()
+        self.stop_event = Event()
+        self.clip_process = Process(
+            target=self.create_clip_process, args=(self.clip_queue, self.stop_event)
+        )
+        self.clip_process.start()
+
+    def create_clip_process(self, queue, stop_event):
+        """
+        Create a clip process using ffmpeg. It consumes clip requests from a queue.
+
+        Args:
+            queue (Queue): The queue of clips.
+            stop_event (Event): The stop event for the clip process.
+
+        Returns:
+            None
+        """
+        # If the user does ctrl+c, stop this subprocess
+        while not stop_event.is_set():
+            try:
+                clip_start_time, clip_end_time, clip_count, video_path = queue.get()
+
+                # Check if there are any more clip requests waiting in the queue
+                if clip_start_time is None:
+                    break
+
+                # Create a clip for the given detection period with ffmpeg
+                clip_duration = clip_end_time - clip_start_time
+                clip_filename = f"{get_clips_dir(video_path)}/{(clip_count):03}-{format_time(clip_start_time, '_')}-{format_time(clip_end_time, '_')}{os.path.splitext(video_path)[1]}"
+                os.makedirs(os.path.dirname(clip_filename), exist_ok=True)
+                subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-hide_banner",
+                        "-loglevel",
+                        "error",
+                        "-y",
+                        "-ss",
+                        str(clip_start_time),
+                        "-t",
+                        str(clip_duration),
+                        "-i",
+                        video_path,
+                        clip_filename,
+                    ]
+                )
+            except KeyboardInterrupt:
+                pass
+            except Empty:
+                continue
+
+    def create_clip(self, clip_start_time, clip_end_time, clip_count, video_path):
+        """
+        Add a clip request to the queue, which will eventually
+        create a new clip file.
+
+        Args:
+            clip_start_time (float): The start time of the clip.
+            clip_end_time (float): The end time of the clip.
+            clip_count (int): The count of the clip.
+            video_path (str): The path to the video file.
+
+        Returns:
+            None
+        """
+        self.logger.debug(
+            f"Creating clip {clip_count}: start={format_time(clip_start_time)}, end={format_time(clip_end_time)}"
+        )
+        self.clip_queue.put((clip_start_time, clip_end_time, clip_count, video_path))
+
+    def stop(self):
+        """
+        Let the queue know it's time to stop processing new clip
+        requests so we can quit.
+
+        Returns:
+            None
+        """
+        self.stop_event.set()
+
+    def cleanup(self):
+        """
+        Wait for the ffmpeg clip queue to complete whatever job(s)
+        are happening now, before we exit.
+
+        Returns:
+            None
+        """
+        if self.clip_queue is not None:
+            self.logger.debug("Waiting for remaining clips to be saved")
+            self.clip_queue.put((None, None, None, None))
+            self.clip_process.join()
---
+++ megadetector_detector.py
@@ -0,0 +1,191 @@
+"""
+This module contains the implementation of the MegadetectorDetector class, which
+inherits from the BaseDetector class. It uses the Megadetector ONNX model for 
+object detection and includes functions for non-max suppression and post-processing 
+of the model's outputs.
+"""
+
+import numpy as np
+from onnxruntime.capi._pybind_state import get_available_providers
+
+from base_detector import BaseDetector
+
+# The Megadetector ONNX model should be in the same directory as this file
+megadetector_model_path = "md_v5a_1_3_640_640_static.onnx"
+# Input images should be 640 x 640
+megadetector_image_width = 640
+megadetector_image_height = 640
+
+
+# Code inspired by https://github.com/parlaynu/megadetector-v5-onnx, used under MIT License
+# https://github.com/parlaynu/megadetector-v5-onnx/blob/main/LICENSE
+
+
+def _calc_ious(b0, bx):
+    """
+    Calculate Intersection over Union (IoU) for two bounding boxes.
+
+    Args:
+        b0 (numpy array): First bounding box.
+        bx (numpy array): Second bounding box.
+
+    Returns:
+        float: IoU value.
+    """
+    # intersection area: (max(0, min(bottom) - max(top))) * (max(0, min(right) - max(left)))
+    i_area = np.maximum(
+        np.minimum(b0[2:4], bx[..., 2:4]) - np.maximum(b0[:2], bx[..., :2]), 0
+    ).prod(axis=1)
+
+    # union area: area_b0 + area_bx - intersection area
+    u_area = (
+        (b0[2:4] - b0[:2]).prod(axis=0)
+        + (bx[..., 2:4] - bx[..., :2]).prod(axis=-1)
+        - i_area
+    )
+
+    return i_area / u_area
+
+
+def _nms(pred, iou_thresh, npred):
+    """
+    Perform non-max suppression on the predictions.
+
+    Args:
+        pred (numpy array): Predictions from the model.
+        iou_thresh (float): IoU threshold for suppression.
+        npred (list): List to store the predictions after suppression.
+
+    Returns:
+        list: Predictions after non-max suppression.
+    """
+    if len(pred) == 0:
+        return npred
+
+    p0 = pred[0]
+    px = pred[1:]
+
+    npred.append(p0)
+    if len(px) == 0:
+        return npred
+
+    # create the IOUs
+    ious = _calc_ious(p0, px)
+
+    # for non-matching class, set the iou to 0 so they don't get considered
+    ious[px[..., 5] != p0[5]] = 0
+
+    # get the remaining predictions
+    pp = px[ious < iou_thresh]
+
+    return _nms(pp, iou_thresh, npred)
+
+
+def non_max_suppression(pred, conf_thresh=0.25, iou_thresh=0.45):
+    """
+    Perform non-max suppression on the predictions and filter them based on
+    confidence threshold.
+
+    Args:
+        pred (numpy array): Predictions from the model.
+        conf_thresh (float, optional): Confidence threshold for filtering. Defaults to 0.25.
+        iou_thresh (float, optional): IoU threshold for suppression. Defaults to 0.45.
+
+    Returns:
+        list: Filtered predictions after non-max suppression.
+    """
+    # prediction: x, y, w, h, box_conf, cls0_conf, cls2_cnf, ...
+    # filter (by box conf_thresh) and sort the predictions
+    pred = pred[pred[..., 4] > conf_thresh]
+    pred = pred[np.flip(np.argsort(pred[..., 4], axis=-1), axis=0)]
+
+    # replace class prob with class label
+    pred[..., 5] = np.argmax(pred[..., 5:], axis=-1)
+    pred = pred[..., :6]
+
+    # convert boxes to xyxy
+    pred[..., :4] = _xywh2xyxy(pred[..., :4])
+
+    # run the nms
+    return _nms(pred, iou_thresh, [])
+
+
+def _xywh2xyxy(xywh):
+    """
+    Convert bounding box format from center x, center y, width, height to
+    top-left x, top-left y, bottom-right x, bottom-right y.
+
+    Args:
+        xywh (numpy array): Bounding box in center x, center y, width, height format.
+
+    Returns:
+        numpy array: Bounding box in top-left x, top-left y, bottom-right x, bottom-right y format.
+    """
+    xyxy = np.zeros_like(xywh)
+    xc, yc, half_w, half_h = xywh[:, 0], xywh[:, 1], xywh[:, 2] / 2, xywh[:, 3] / 2
+    xyxy[:, 0] = xc - half_w
+    xyxy[:, 1] = yc - half_h
+    xyxy[:, 2] = xc + half_w
+    xyxy[:, 3] = yc + half_h
+    return xyxy
+
+
+class MegadetectorDetector(BaseDetector):
+    """
+    MegadetectorDetector class that inherits from BaseDetector. It uses the
+    Megadetector ONNX model for object detection and includes methods for
+    post-processing of the model's outputs.
+    """
+
+    def __init__(self, logger):
+        """
+        Initialize the MegadetectorDetector class.
+
+        Args:
+            logger (Logger): Logger object for logging.
+        """
+        logger.info(
+            "Initializing Megadetector Model and Optimizing (this will take a minute...)"
+        )
+        providers = get_available_providers()
+        super().__init__(
+            logger,
+            megadetector_model_path,
+            megadetector_image_width,
+            megadetector_image_height,
+            "Animal",
+            providers,
+        )
+
+    def post_processing(self, outputs, confidence_threshold):
+        """
+        Perform post-processing on the model's outputs. This includes non-max
+        suppression, filtering based on confidence threshold, and conversion of
+        absolute coordinates to relative.
+
+        Args:
+            outputs (numpy array): Outputs from the model.
+            confidence_threshold (float): Confidence threshold for filtering.
+
+        Returns:
+            list: Post-processed predictions.
+        """
+        preds = []
+        for p in outputs[0]:
+            # TODO: what to use for IOU_THRESHOLD? Defaults to 0.45 in run-onnx.py
+            p = non_max_suppression(p, confidence_threshold, 0.45)
+            # Filter out predictions for animals (class=0)
+            p = [pred for pred in p if pred[5] == 0]
+            # If there are predictions, convert absolute coordinates to relative
+            if len(p) > 0:
+                p = np.array(p)
+                p[..., :4] = p[..., :4] / [
+                    self.input_image_width,
+                    self.input_image_height,
+                    self.input_image_width,
+                    self.input_image_height,
+                ]
+
+            preds.append(p)
+
+        return preds
