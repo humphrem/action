d09fac7ab8d450622227c39eb125277a1fb3422b
David Humphrey <david.andrew.humphrey@gmail.com>
2023-10-30 21:25:29 -0400

    Updates for 1.0.1 release, fix aquatic-demo.sh, add citation.cff

--- scripts/aquatic-demo.sh
+++ scripts/aquatic-demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 action.py ./video/aquatic_demo_1.mov -c 0.45 -m 3.0 -s -b 1.0 -d -e aquatic
+python3 action.py ./video/aquatic_demo_1.mov -c 0.70 -m 3.0 -s -i -b 1.0 -d -e aquatic
